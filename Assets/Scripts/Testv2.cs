2794e1e5e8e95bf7b23676b82c55b9d7be8712d7
Mark Ballinger <markballinger@optusnet.coml.au>
2023-04-05 10:28:46 +1000

    new script updated

--- Assets/Scripts/Testv2.cs
+++ Assets/Scripts/Testv2.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,7 +8,16 @@ public class Testv2 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Console.WriteLine("farking work cunt");
+        Console.WriteLine("farking work cunt");
+        Console.WriteLine("farking work cunt");
+        Console.WriteLine("farking work cunt");
+        Console.WriteLine("farking work cunt");
+        Console.WriteLine("farking work cunt");
+        Console.WriteLine("farking work cunt");
+        Console.WriteLine("farking work cunt");
+        Console.WriteLine("farking work cunt");
+        Console.WriteLine("farking work cunt");
     }
 
     // Update is called once per frame
